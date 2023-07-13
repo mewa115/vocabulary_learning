b0227cc1abc90219509b634353c98c9aed114d59
mykytameshcheryakov <nik.meshcheryakov@gmail.com>
2023-07-13 16:55:41 +0300

    initial commit

--- data_managemen.py
+++ data_managemen.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import csv
+import random
 
 # Open the file
 rows = 0
@@ -29,3 +30,26 @@ print(type(specific_row))
 
 list_of_lists = df.values.tolist()
 print(list_of_lists)
+print('-----------')
+
+random_word = list_of_lists[random.randint(0, len(list_of_lists))]
+
+
+
+i = 1
+random_numbers_list = list()
+
+while i < 11:
+    random_number = random.randint(0, len(list_of_lists))
+    while random_number in random_numbers_list:
+        random_number = random.randint(0, len(list_of_lists))
+    random_numbers_list.append(random_number)
+    i = i + 1
+print(random_numbers_list)
+
+list_for_the_current_lesson = list()
+
+for each in random_numbers_list:
+    list_for_the_current_lesson.append(list_of_lists[each])
+print(list_for_the_current_lesson[1])
+
