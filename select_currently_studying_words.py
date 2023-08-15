b7304db002f31a015bc34e35d2970496eb7bb657
mykytameshcheryakov <nik.meshcheryakov@gmail.com>
2023-08-15 18:34:45 +0300

    I am in the process of adding the logic to keep 50 words in the learning batch

--- select_currently_studying_words.py
+++ select_currently_studying_words.py
@@ -4,19 +4,35 @@ import random
 original_file = 'Saved_translations_orig.csv'
 copy_file = 'Saved_translations_copy.csv'
 
-df_select_currently_studying = pd.read_csv(copy_file)
+df_select_currently_studying = pd.read_csv(copy_file, index_col=None)
 # calculate the number of rows in the file
 num_rows = df_select_currently_studying.shape[0]
 
 # generate 50 numbers from the range
-random_indexes = random_numbers = random.sample(range(0, num_rows), 50)
 
-# set the value to TRUE for the words, which I want to learn. I need to fins the row with the index from the
-# random_indexes and set the TRUE value to the last column
-for each in random_indexes:
-    if df_select_currently_studying.loc[each:each, 'already_learned'] ==  False:
-        df_select_currently_studying.loc[each:each, 'currently_studying'] = True
-df_select_currently_studying.to_csv(copy_file)
+
+
+
+random_indexes = random_numbers = random.sample(range(0, num_rows), 50)
 
 number_total = df_select_currently_studying['word_from'].notna().sum()
 print(number_total)
+
+# calculate the number of words, which are currently in the lesson. The value should be 50.
+count_true = df_select_currently_studying[df_select_currently_studying['currently_studying'] == True].shape[0]
+
+if count_true < 50:
+    difference = 50 - count_true
+    for each in range(1,difference):
+        if df_select_currently_studying.loc[each:each, 'already_learned'].values[0] == False:
+            if df_select_currently_studying.loc[each:each, 'currently_studying'].values[0] == False:
+                df_select_currently_studying.loc[each:each, 'currently_studying'] = True
+else:
+    for each in random_indexes:
+        if df_select_currently_studying.loc[each:each, 'already_learned'].values[0] == False:
+            if df_select_currently_studying.loc[each:each, 'currently_studying'].values[0] == False:
+                df_select_currently_studying.loc[each:each, 'currently_studying'] = True
+
+
+print(count_true)
+
