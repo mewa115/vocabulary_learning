aa1974617c700c0975b885dc493e95271991afa8
mykytameshcheryakov <nik.meshcheryakov@gmail.com>
2023-08-16 15:00:13 +0300

    Now, we can select the words to learn and we can keep always 50 words which are being learned

--- select_currently_studying_words.py
+++ select_currently_studying_words.py
@@ -4,32 +4,39 @@ import random
 original_file = 'Saved_translations_orig.csv'
 copy_file = 'Saved_translations_copy.csv'
 
-#read the csv file
-df_select_currently_studying = pd.read_csv(copy_file, index_col=None)
+# read the csv file
+df_select_currently_studying = pd.read_csv(copy_file, index_col=None, header=0)
 
-# calculate the number of words, which are currently in the lesson. The value should be 50.
-count_true = df_select_currently_studying[df_select_currently_studying['currently_studying'] == True].shape[0]
-print('initial count_true is ', count_true)
+# get the list of indexes of all words in the file
+all_indexes = df_select_currently_studying.index.tolist()
+# get the list of indexes of currently studying words in the file
 
-if count_true == 0:
-    # calculate the number of rows in the file. The attribute shape[0] - returns the number of rows and
-    # shape[1] returns the number of columns.
-    num_rows = df_select_currently_studying.shape[0]
-    print('total number of rows is ', num_rows)
-    # generate 50 numbers from the range
-    random_indexes = random.sample(range(0, num_rows), 50)
-    print('random_indexes are ', random_indexes)
-    for each in random_indexes:
-        if df_select_currently_studying.loc[each:each, 'already_learned'].values[0] == False:
-            if df_select_currently_studying.loc[each:each, 'currently_studying'].values[0] == False:
-                df_select_currently_studying.loc[each:each, 'currently_studying'] = True
-# if count_true < 50:
-#     difference = 50 - count_true
-#     for each in range(1,difference+1):
-#         if df_select_currently_studying.loc[each:each, 'already_learned'].values[0] == False:
-#             if df_select_currently_studying.loc[each:each, 'currently_studying'].values[0] == False:
-#                 df_select_currently_studying.loc[each:each, 'currently_studying'] = True
+indexes_of_currently_studying_words = \
+    df_select_currently_studying[df_select_currently_studying['currently_studying'] == True].index.tolist()
+try:
+    count_the_indexes_of_currently_studying_words = indexes_of_currently_studying_words.count()
+except TypeError:
+    count_the_indexes_of_currently_studying_words = 0
+# get the indexes of alreadt learned words
+indexes_of_already_learned_words = df_select_currently_studying[
+    df_select_currently_studying['already_learned'] == True].index.tolist()
+# calculate the total number of all learned words
+try:
+    count_of_already_learned_words = indexes_of_already_learned_words.count()
+except TypeError:
+    count_of_already_learned_words = 0
 
-
-check_count_true = df_select_currently_studying[df_select_currently_studying['currently_studying'] == True].shape[0]
-print('check_count_true is ', check_count_true)
+# get the indexes of the words, which are available for pickup to learn, i.e. the words which are
+# not currently being learned or words which have been already learned
+indexes_of_available_to_learn_words = [x for x in all_indexes if
+                                       x not in indexes_of_currently_studying_words and x not in indexes_of_already_learned_words]
+if count_the_indexes_of_currently_studying_words == 0:
+    indexes_of_new_to_learn_words = random.sample(indexes_of_available_to_learn_words, 50)
+        # pick random list of indexes of the words, which are available for learn
+    for each in indexes_of_new_to_learn_words:
+        df_select_currently_studying[each, 'currently_studying'] = True
+elif count_the_indexes_of_currently_studying_words < 50:
+    indexes_of_new_to_learn_words = random.sample(indexes_of_available_to_learn_words, 50 - (
+            count_of_already_learned_words + indexes_of_available_to_learn_words))
+    for each in indexes_of_new_to_learn_words:
+        df_select_currently_studying[each,'currently_studying'] = True
